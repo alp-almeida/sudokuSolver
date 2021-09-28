e2f2ab165cf62c57292691ed7275f2adb9e56d80
André Luiz Pereira de Almeida <andre_almeida15@hotmail.com>
2021-09-28 00:35:52 -0300

    Update sudoku_back_app.py

--- app/sudoku_back_app.py
+++ app/sudoku_back_app.py
@@ -4,14 +4,14 @@ def criar_campo():
     campo = 0
     return campo
 
-def criar_linha():
-    linha = [criar_campo()]*9
+def criar_linha(nCampos = 9):
+    linha = [criar_campo()] * nCampos
     return linha
     
-def criar_tabuleiro():
+def criar_tabuleiro(nLinhas = 9):
     tabuleiro_CT = []
-    for i in range(0,9):
-        tabuleiro_CT.append(criar_linha())
+    for i in range(0,nLinhas):
+        tabuleiro_CT.append(criar_linha(nLinhas))
         #print tabuleiro_CT
     return tabuleiro_CT
     
