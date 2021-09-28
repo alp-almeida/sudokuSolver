52f43a0159f3728db0b90e3f16a9a38c240e842c
André Luiz Pereira de Almeida <andre_almeida15@hotmail.com>
2021-09-28 00:36:23 -0300

    Update sudokuApp.py

--- app/sudokuApp.py
+++ app/sudokuApp.py
@@ -5,8 +5,10 @@ import sudoku_back_vitoria as v
 def main():
     tabuleiro = b.criar_tabuleiro()
     tabuleiro[8][0] = 45
+    tabuleiro[0][8] = 30
+    v.regra_da_vitoria(tabuleiro)
     
-    print v.vitoria_soma_linha(tabuleiro)
+    print tabuleiro[0][6:9]
 
 
 
